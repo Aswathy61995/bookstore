24a4af36e143c42c58dad665133b64a36b69ca2c
Aswathy61995 <72978903+Aswathy61995@users.noreply.github.com>
2021-03-12 09:42:12 +0530

    Add files via upload
    updated file

---
+++ New folder (2)/frontend/src/ProductstylingController.js
@@ -0,0 +1,45 @@
+
+import React, { useState }  from "react";
+
+import{Link} from 'react-router-dom'
+  import BellIcon from 'react-bell-icon';
+
+function ProductstylingController({ booksObject,title, price, imagePath,}) {
+    
+   const alertName=()=>{
+     
+       alert('Book Added to cart' )
+   };
+//    const [cart,setCart]= useState([]);//
+//alert(booksObject.title);
+return (
+    
+     <div className="product">
+        
+   <p className="imagePath">
+        <img src= {imagePath} width="150px" height="250px"></img>
+            <Link to =
+                   {{ 
+                    pathname: "/getBooksByTitle",
+                   booksObject:booksObject,
+                   // title:"title",
+                    //price: booksObject.price,
+                    //imagePath:booksObject.imagePath
+                    }}>
+                          <p>{title}</p> 
+ </Link>
+        </p>
+       <div className="product__info">
+      
+        </div>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+         </p>
+       <button onClick={alertName} > Add to Basket</button>
+      </div>
+     
+  );
+
+                }
+export default ProductstylingController;
